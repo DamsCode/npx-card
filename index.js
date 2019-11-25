a1c0857b9136a381de43d7d782345e083f7c3b6e
damien <d.macors@gmail.com>
2019-11-25 14:51:51 +0100

    mycard1

---
+++ index.js
@@ -0,0 +1,21 @@
+const figlet = require('figlet');
+
+const chalk = require('chalk');
+figlet('Damien Macors', function(err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log(data);
+    let npm = "https://www.npmjs.com/~damscode";
+    let gh = "https://github.com/DamsCodes";
+    let lk = "https://www.linkedin.com/in/damien-macors-b4915028/";
+    console.log(chalk`
+         {bgRed.whiteBright npm}: ${npm}
+      GitHub: ${gh}
+    {bgBlue LinkedIn}: ${lk}
+`);
+});
+
+
